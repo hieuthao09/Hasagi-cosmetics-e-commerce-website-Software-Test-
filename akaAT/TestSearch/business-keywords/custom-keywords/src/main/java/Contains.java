5abb7553c019052ffdb59d5b4f669a6f7141ba1b
Thao909 <hieuthaowork00@gmail.com>
2024-01-18 16:18:37 +0700

    First commit

---
+++ akaAT/TestSearch/business-keywords/custom-keywords/src/main/java/Contains.java
@@ -0,0 +1,5 @@
+public class Contains {
+    public Boolean ContainString(String input1, String input2) {
+        return input1.contains(input2);
+    }
+}
---
+++ akaAT/TestSearch/business-keywords/custom-keywords/src/main/java/Hello.java
@@ -0,0 +1,6 @@
+ 
+public class Hello {
+    public boolean contain(String input1, String input2) {
+        return input1.contains(input2);
+    }
+}
